f18325728a4ce1eb5f62413f880a1f49d7f5e0db
alexeyyauk <alexeyyauk52@gmail.com>
2026-05-13 23:03:45 +0500

    1

---
+++ src/main/java/com/alexey/textmod/Textmod.java
@@ -0,0 +1,12 @@
+package com.alexey.textmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Textmod implements ModInitializer
+{
+
+    @Override
+    public void onInitialize()
+    {
+    }
+}
---
+++ src/main/java/com/alexey/textmod/client/TextmodClient.java
@@ -0,0 +1,12 @@
+package com.alexey.textmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TextmodClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
