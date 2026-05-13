5b4110782da8162decdec331a1a7cc8535fdaee6
alexeyyauk <alexeyyauk52@gmail.com>
2026-05-13 23:15:49 +0500

    scam fix

--- src/main/java/com/alexey/textmod/Textmod.java
+++ src/main/java/com/alexey/textmod/Textmod.java
@@ -1,12 +1,18 @@
 package com.alexey.textmod;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
-public class Textmod implements ModInitializer
+public class Textmod implements ModInitializer, ClientModInitializer
 {
 
     @Override
     public void onInitialize()
     {
     }
+    @Override
+    public void onInitializeClient()
+    {
+    }
+
 }
